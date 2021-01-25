2568726d731797e028ef0c5fe3aee3cbb7d1344d
Mauro Passerino <mpasserino@irobot.com>
2021-01-25 11:33:19 -0300

    Implement events executor on Foxy

--- rmw_implementation/src/functions.cpp
+++ rmw_implementation/src/functions.cpp
@@ -601,6 +601,36 @@ RMW_INTERFACE_FN(
     bool,
     rmw_topic_endpoint_info_array_t *))
 
+RMW_INTERFACE_FN(
+  rmw_subscription_set_listener_callback,
+  rmw_ret_t, RMW_RET_ERROR,
+  4, ARG_TYPES(
+    rmw_subscription_t *, rmw_listener_cb_t, const void *, const void *))
+
+RMW_INTERFACE_FN(
+  rmw_service_set_listener_callback,
+  rmw_ret_t, RMW_RET_ERROR,
+  4, ARG_TYPES(
+    rmw_service_t *, rmw_listener_cb_t, const void *, const void *))
+
+RMW_INTERFACE_FN(
+  rmw_client_set_listener_callback,
+  rmw_ret_t, RMW_RET_ERROR,
+  4, ARG_TYPES(
+    rmw_client_t *, rmw_listener_cb_t, const void *, const void *))
+
+RMW_INTERFACE_FN(
+  rmw_guard_condition_set_listener_callback,
+  rmw_ret_t, RMW_RET_ERROR,
+  5, ARG_TYPES(
+    rmw_guard_condition_t *, rmw_listener_cb_t, const void *, const void *, bool))
+
+RMW_INTERFACE_FN(
+  rmw_event_set_listener_callback,
+  rmw_ret_t, RMW_RET_ERROR,
+  5, ARG_TYPES(
+    rmw_event_t *, rmw_listener_cb_t, const void *, const void *, bool))
+
 #define GET_SYMBOL(x) symbol_ ## x = get_symbol(#x);
 
 void prefetch_symbols(void)
@@ -678,6 +708,11 @@ void prefetch_symbols(void)
   GET_SYMBOL(rmw_set_log_severity)
   GET_SYMBOL(rmw_get_publishers_info_by_topic)
   GET_SYMBOL(rmw_get_subscriptions_info_by_topic)
+  GET_SYMBOL(rmw_subscription_set_listener_callback)
+  GET_SYMBOL(rmw_service_set_listener_callback)
+  GET_SYMBOL(rmw_client_set_listener_callback)
+  GET_SYMBOL(rmw_guard_condition_set_listener_callback)
+  GET_SYMBOL(rmw_event_set_listener_callback)
 }
 
 void * symbol_rmw_init = nullptr;
